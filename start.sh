deedb2113b6e3b421307849e64a0cdba76018e97
Logan Kessler <logankessler@ku.edu>
2025-11-09 10:49:32 -0600

    Final State of Raspberry Pi

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # Launch Flask app with environment variables
-
 export FLASK_HOST=192.168.4.1
 export FLASK_PORT=80
 export DB_PATH=./data.sqlite          # default
 export CSV_PATH=./submissions.csv     # default
 export ADMIN_TOKEN=supersecret123     # change this before deploying
-python app.py
 
+#firefox --kiosk "http://localhost/admin" &
+#.venv/bin/python3 app.py &
+#source .venv/bin/activate
+sudo python app.py &
+#sleep 5
+chromium --kiosk "http://localhost/admin" &
 
 echo "Flask app started with the following settings:"
 echo "Host: $FLASK_HOST"
